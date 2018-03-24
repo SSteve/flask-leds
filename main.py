a2caf3790c28c7689237fd2736ccc40732a81e54
Steve Nicholson <ssteve@mac.com>
2018-03-24 10:13:52 -0700

    Simplify to basics

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from ledstrip import ledstrip
-import views
 
 app = Flask(__name__)
 leds = ledstrip()
 
+@app.route('/')
+def homepage():
+	return render_template('homepage.html')
+
+@app.route('/on', methods=['POST'])
+def on():
+	leds.red_on()
+	return redirect('/')
+
+@app.route('/off', methods=['POST'])
+def off():
+	leds.red_off()
+	return redirect('/')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
-	
+
